1683441c5675728ad779e4fcb5a55c8403264a79
navnit0707 <knavnit81@gmail.com>
2022-11-29 19:20:51 +0530

    using useState to catch error and set loading functionlaities

--- project/src/App.js
+++ project/src/App.js
@@ -1,19 +1,40 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
+function GithubUser({name,location}){
+  return(
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+    </div>
+  );
+}
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading , setLoading] = useState(true);
   useEffect(()=>{
+    setLoading(true);
     fetch(`https://api.github.com/users/navnit0707`)
     .then((respose)=> respose.json())
-    .then(data=>setData(data));
+    .then(data=>setData(data))
+    .then(()=>setLoading(false))
+    .catch(setError);
   },[]);
   
-  if(data)
-  return(<pre>{JSON.stringify(data,null, 2)}</pre>);
+  
+  if(loading){
+    return <h1>loading...</h1>
+  }
 
-  return <h1>Data</h1>
+  if(error){
+    return <h1>error</h1>
+  }
+
+  return(
+    <GithubUser name={data.name} location={data.location}/>
+  );
 }
 
 export default App;
